ef33316884d453be867a2fc1f60d7878ba23d561
Faizalilham <faizalfalakh19@gmail.com>
2022-09-04 16:01:13 +0700

    Tambahan tambah panggilan

--- app/src/main/java/com/example/viewpager/fragment/BaseFragment.kt
+++ app/src/main/java/com/example/viewpager/fragment/BaseFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.viewpager.R
 import com.example.viewpager.adapter.ViewPagerAdapter
 import com.example.viewpager.databinding.FragmentBaseBinding
+import com.google.android.material.tabs.TabLayout
 
 
 class BaseFragment : Fragment() {
@@ -20,8 +24,21 @@ class BaseFragment : Fragment() {
         binding = FragmentBaseBinding.inflate(layoutInflater)
         val viewPagerAdapter = ViewPagerAdapter(childFragmentManager)
         binding.viewPager.adapter = viewPagerAdapter
-        binding.Tablayout.setupWithViewPager(binding.viewPager)
+        binding.viewPager.setCurrentItem(1,true)
+        binding.Tablayout.apply {
+            setupWithViewPager(binding.viewPager)
+            getTabAt(0)?.setIcon(R.drawable.ic_camera)
+            setTabWidthAsWrapContent(0)
+        }
         return binding.root
+    }
+
+    private fun TabLayout.setTabWidthAsWrapContent(tabPosition: Int) {
+        val layout = (this.getChildAt(0) as LinearLayout).getChildAt(tabPosition) as LinearLayout
+        val layoutParams = layout.layoutParams as LinearLayout.LayoutParams
+        layoutParams.weight = 0f
+        layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT
+        layout.layoutParams = layoutParams
     }
 
 
